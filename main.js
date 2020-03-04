79350cee9c0b80191489763e4235129eea1b5692
edwwin <58546917+edwwin@users.noreply.github.com>
2020-03-04 20:57:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const pageNavDesktop = document.querySelector(".page-nav-desktop");
+const pageNavMobile = document.querySelector(".page-nav-mobile");
+
+window.addEventListener("scroll", e => {
+  if (window.scrollY > 150) {
+    window.requestAnimationFrame(() => {
+      // smallNav(pageNavDesktop);
+      // smallNav(pageNavMobile);
+    });
+  } else if (window.scrollY < 100) {
+    window.requestAnimationFrame(() => {
+      // bigNav(pageNavDesktop);
+      // bigNav(pageNavMobile);
+    });
+  }
+});
+
+// This is the important part!
+
+function smallNav(element) {
+  const sectionHeight = element.scrollHeight;
+
+  requestAnimationFrame( () => {
+    element.style.height = "40px";
+    
+    requestAnimationFrame( () => {
+      element.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    })
+  });
+}
+
+function bigNav(element) {
+  const sectionHeight = element.scrollHeight;
+
+  requestAnimationFrame( () => {
+    element.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  });
+
+  element.style.height = sectionHeight + "px";
+}
